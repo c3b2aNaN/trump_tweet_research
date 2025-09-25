a72229dd15ba3897c43bfca3ec39ef5bbe61c7d8
c3b2aNaN <zwfzwf003@gmail.com>
2025-09-25 09:42:19 -0400

    update clusters

--- Clusters/convert_mallet.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-fn = sys.argv[1]
-fn_noext = ".".join(fn.split(".")[:-1])
-
-cmd = f"mallet import-file --input {fn} --output {fn_noext}.mallet --keep-sequence --remove-stopwords"
-
-import os
-os.system(cmd)
